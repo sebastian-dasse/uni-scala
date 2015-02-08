ab6e6ee0fb4953b575e7d9c45614dd4c4f20cf75
sebastian-dasse <s50602@beuth-hochschule.de>
2015-02-08 12:55:59 +0100

    probeklausur, first try

---
+++ ScalaKurs(Knabe)_S_sbt/src/main/scala/count/Counter.scala
@@ -0,0 +1,27 @@
+package count
+
+import akka.actor.ActorLogging
+import akka.actor.Actor
+
+object Counter {
+  case class Number(n: Int)
+  case object Finish
+  case class Report(evens: Int, odds: Int)
+}
+
+class Counter extends Actor with ActorLogging {
+  
+  import Counter._
+  
+  var evens = 0
+  var odds = 0
+  
+  def receive(): Receive = {
+    case Number(n) =>
+      val even = n % 2 == 0
+      val clazz = if (even) "even " else "odd"
+	  log.info(s"Number $n is $clazz")
+      if (even) evens += 1 else odds += 1
+    case Finish => sender ! Report(evens, odds)
+  }
+}
